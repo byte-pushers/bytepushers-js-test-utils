53b998f737af931bf938ac174e3a1ce8212a52a5
kenFlowers <kenneth_flow@Hotmail.com>
2017-09-28 12:33:50 -0500

    Added test to call public methods. refactored private method test

--- src/test/javascript/Tester.spec.js
+++ src/test/javascript/Tester.spec.js
@@ -4,17 +4,37 @@
 define(['bytepushers', 'Employee'], function(BytePushers) {
     describe("Tester", function(){
         it('should be able to call private methods', function () {
-            var tester = (new BytePushers.Tester()).getInstance(Employee);
-            expect(tester).toBeDefined();
-            expect(tester._privates).toBeDefined();
-            expect(tester._privates["getFirstName"]).toBeDefined();
-            expect(tester._privates.getFirstName()).toEqual("Sally");
-            expect(tester._privates.calculateAge).toBeDefined();
-            expect(tester._privates.calculateAge()).toEqual(20);
+            var emp = (new BytePushers.Tester()).getInstance(Employee);
+            expect(emp).toBeDefined();
+            expect(emp._privates).toBeDefined();
+            expect(emp._privates["getFirstName"]).toBeDefined();
+            expect(emp._privates.getFirstName()).toEqual("Sally");
+            expect(emp._privates["getMiddleName"]).toBeDefined();
+            expect(emp._privates.getMiddleName()).toEqual("Mae");
+            expect(emp._privates["getLastName"]).toBeDefined();
+            expect(emp._privates.getLastName()).toEqual("Sue");
+            expect(emp._privates.calculateAge).toBeDefined();
+            expect(emp._privates.calculateAge()).toEqual(20);
+            expect(emp._privates.getSalary).toBeDefined();
+            expect(emp._privates.getSalary()).toEqual(50000);
         });
 
 
         //TODO: Create another test that shows that tester can call public metods.
+       it('should be able to call public methods', function () {
+            var emp = (new BytePushers.Tester()).getInstance(Employee);
+
+            expect(emp.getFullName).toBeDefined();
+            expect(emp.getFullName()).toEqual("Sally Mae Sue");
+            expect(emp.getDepartment).toBeDefined();
+            expect(emp.getDepartment()).toEqual("Human Resources");
+            expect(emp.getAge).toBeDefined();
+            expect(emp.getAge()).toEqual(20);
+            expect(emp.getType).toBeDefined();
+            expect(emp.getType()).toEqual("full-time");
+
+
+       });
     });
 
     describe("Employee class", function () {
@@ -36,7 +56,6 @@ define(['bytepushers', 'Employee'], function(BytePushers) {
 
         it('should not be able to call private methods', function () {
             var actualBirthDate = "07/2/1970";
-            var expectedAge = new Date().getFullYear() - new Date(actualBirthDate).getFullYear();
             var emp = new Employee(null, "M", undefined, actualBirthDate, null , "part-time", undefined);
 
             expect(emp.getFirstName).not.toBeDefined();
